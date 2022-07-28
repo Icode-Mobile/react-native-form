5e0a7113e324a15f20a54fa8d805cbcc42b877a8
Caio18-cosenza <caiiocosenza2020@gmail.com>
2022-07-28 14:34:22 -0300

    layout design

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,36 +1,17 @@
 import { useState } from 'react';
 import { Alert } from 'react-native';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 
 import { Button } from '../Button';
 import { Input } from '../Input';
 
 import { Container, Title } from './style';
 
-const schema = yup.object({
-  email: yup.string().email().required(),
-  password: yup.string().required(),
-});
-
-type InputProps = {
-  email: string;
-  password: string;
-};
-
 export const Form = () => {
-  const {
-    handleSubmit,
-    formState: { errors },
-    register,
-  } = useForm<InputProps>({
-    resolver: yupResolver(schema),
-  });
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   const handleLogin = () => {
-    // Alert.alert('Seus dados', `Email: ${email}\nSenha: ${password}`);
-    alert('ok');
+    Alert.alert('Seus dados', `Email: ${email}\nSenha: ${password}`);
   };
 
   return (
@@ -39,23 +20,26 @@ export const Form = () => {
       <Input
         placeholder='Email'
         placeholderTextColor={'#777'}
-        keyboardType='email-address'
-        {...register('email')}
         autoCapitalize={'none'}
+        autoCorrect={false}
+        onChangeText={(t) => setEmail(t.includes(' ') ? '' : t)}
+        importantForAutofill='no'
       />
       <Input
         placeholder='Senha'
         placeholderTextColor={'#777'}
         secureTextEntry
-        {...register('password')}
         autoCapitalize={'none'}
+        onChangeText={(t) => setPassword(t.includes(' ') ? '' : t)}
+        autoCorrect={false}
+        importantForAutofill='no'
       />
       <Button
         title='Entrar'
         activeOpacity={0.6}
-        onPress={handleSubmit(handleLogin)}
-        disabled={errors ? true : false}
-        style={{ backgroundColor: errors ? '#222' : '#1c59ba' }}
+        onPress={handleLogin}
+        disabled={!email || !password}
+        style={{ backgroundColor: !email || !password ? '#222' : '#1c59ba' }}
       />
     </Container>
   );
